92603eac04af1c519f3c2b2f72b935748c2b487e
灶门炭治郎的歌曲 <chacrit.p@kkumail.com>
2025-10-15 11:59:32 +0700

    c

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
 driver = webdriver.Chrome()
-driver.get("https://www.google.com")
+driver.get("https://www.agoda.com/th-th/baron-beach-hotel/hotel/pattaya-th.html?ds=lQ6UVmoPoGPBue45")
 print(driver.title)
 driver.quit()
